a17670faa652d5dbd7f9ed8fe2ae2e1e83a0306d
k2 <trofimchuk.an@gmail.com>
2018-10-23 23:58:01 +0300

    Created all routes

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const http = require("http");
 const Main = require("./routes/main");
+const Home = require("./routes/home");
+const Search = require("./routes/search");
+const NotFound = require("./routes/notFound");
 
 http
   .createServer((req, res) => {
     if (req.url.match(/\.(html|css|js|png)$/)) {
       Main(req, res);
     } else if (req.url === "/") {
-      console.log(req.url);
+      Home(req, res);
     } else if (req.url.startsWith("/search")) {
+      Search(req, res);
     } else {
+      NotFound(req, res);
     }
   })
   .listen(80, () => console.log("Running..."));
