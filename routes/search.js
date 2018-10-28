3dbb53c7eb92b09eb59290df26b891e88ad969c2
k2 <trofimchuk.an@gmail.com>
2018-10-28 21:40:27 +0200

    Added basic lyout in movie.html and 404.html

--- routes/search.js
+++ routes/search.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const path = require("path");
-const Request = require('../modules/request');
-const Render = require('../modules/render');
-const url = require('url');
+const Request = require("../modules/request");
+const Render = require("../modules/render");
+const url = require("url");
 
 function Search(req, res) {
   const parsedUrl = url.parse(req.url, true);
@@ -10,17 +10,17 @@ function Search(req, res) {
   console.log(title);
 
   Request(title, (error, movie) => {
-
-    Render('movie.html', movie, (error, html) => {
+    Render("movie.html", movie, (error, html) => {
+      console.log(movie);
       if (error) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.writeHead(500, { "Content-Type": "text/plain" });
         res.end(res.message);
-      };
+      }
       res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
+      res.setHeader("Content-Type", "text/html");
       res.end(html);
-    })
-  })
+    });
+  });
 }
 
 module.exports = Search;
